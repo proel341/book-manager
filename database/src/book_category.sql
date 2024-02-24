825965bb0ffdb7db411b7ad3dfe63ddb3d0a8daa
proel341 <103094854+proel341@users.noreply.github.com>
2024-02-24 19:32:34 +0700

    fix misstakes. Type_category_id is not unique in table book_category

--- database/src/book_category.sql
+++ database/src/book_category.sql
@@ -3,6 +3,6 @@ CREATE TABLE book_category(
     parent_id INT REFERENCES book_category(id),
     creater_id INT NOT NULL REFERENCES booklover(id),
     title TEXT NOT NULL UNIQUE,
-    category_type_id INT NOT NULL UNIQUE REFERENCES category_type(id),
+    category_type_id INT NOT NULL REFERENCES category_type(id),
     created_at TIMESTAMP NOT NULL
 );
